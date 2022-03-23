e8bf963a08e4241e9966241ab09698848300e76e
YOUMBI Harold <80352823+Youmbi-Harold@users.noreply.github.com>
2022-03-23 20:19:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#A program to check the required height for a ride
+print("Welcome to the rollercoaster Guys!")
+height = int(input("What is your height in cm? \n"))
+
+if height > 120:
+  print("Please move foward, You can ride on!")
+else:
+  print("Sorry, You can't have a ride")
